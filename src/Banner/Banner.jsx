2a78e94ce2c6f8193d32a3a2047587462b8e8b28
mehedi hasan <mdchanchal5675@gmail.com>
2023-06-28 13:14:24 +0600

    update mehedi

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,7 +2,8 @@
 const Banner = () => {
     return (
         <div>
-            <h2>hi i am chanchal</h2>
+            <h2>hi i am chanchal.</h2>
+            <h2>hi i am a student</h2>
         </div>
     );
 };
